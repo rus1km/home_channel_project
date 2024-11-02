a6f5194ca14364dd9b081209738ba23be5a05d4a
rus1km <mamedov.rust@gmail.com>
2024-11-02 13:45:24 +0200

    Fix air raid alert logic

--- alerts.py
+++ alerts.py
@@ -8,11 +8,10 @@ load_dotenv()
 
 ALERTS_API_KEY = os.getenv("ALERTS_API_KEY")
 CITY_UID = os.getenv("CITY_UID")
-CITY_NAME = os.getenv("CITY_NAME")
 
 # location UID can be found here https://devs.alerts.in.ua
 def get_air_raid_alert_status():
     alerts_client = AlertsClient(token=ALERTS_API_KEY)
-    status = alerts_client.get_air_raid_alert_status(CITY_UID)
-    return 1 if status == f"active:{CITY_NAME}" else 0
-    
+    response = alerts_client.get_air_raid_alert_status(CITY_UID)
+    status = 1 if response.status == "active" else 0
+    return status
